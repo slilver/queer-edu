46474c8b298998dc8a5e0a4230ae061351f6e075
Kyrie Johnson <slilver3737@gmail.com>
2023-05-22 17:48:10 -0400

    Add placeholder for Kyrie Python code.

---
+++ queer-edu-python/main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Define function that prints simple text output.
+def print_hi(name):
+    print('Hi', name, '!')
+
+
+# If name of script is main, then run.
+if __name__ == '__main__':
+    print_hi('k')
